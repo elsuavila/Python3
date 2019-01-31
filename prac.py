0f2caa97c0a8dcd82374c0147d09e5bf1cb66be1
elsuavila <avilaelsy47@gmail.com>
2019-01-30 23:38:15 -0600

    first commit

---
+++ prac.py
@@ -0,0 +1,78 @@
+#Ejercicio 1
+#Necesitan creaer un programa que les permita selecionar entre una de dos opciones.
+#Convertir a dolores
+#Convertir quetzales a dolares y si ingresa una opcion valida decir opcion incorrecta.
+
+
+#print("Bienvedio al progrma")
+#DOLAR = 7.7
+#CONVERSION 
+#print("Selecionar el tipo de conversion mas util")
+#conversion = int(input("Ingresar primera o segunda opcion"))
+#if convrersion == Numero1:
+#dolar = float(input("Ingrese su cantidad"))
+   # Resultado dolar*dolar
+   # print("La conversion es {}".format(conversion))
+#elif conversion == Segunda opcion:
+    #quetzales = float(input("Ingrese su cantidad")
+       # Resulado dolar/dolar
+       # print("La conversion es {}".format(conversion))
+#else:
+   # print("Valor incorrecto")
+
+
+
+
+
+
+
+#Ejerciicio 2
+#Solicitar al usuario que escoja una de las siguientes opciones
+#1. Sumar dos numeros
+#2. Restar tres numeros
+#3. Muliplicar cuatro numeros
+#4. Dividir dos numeros
+# Si ingresa una opcin invalida hacersel saber.
+
+print("""Escoja una de las catro opciones,para sumar dos numeros opcion 1, para restar tres numeros opcion 2
+      para multipicar cuatro numeros opcion 3, y para dividir dos numeros opcion 4."""
+
+Opcion = int(input("Ingresar opcion 1,2,3,4")
+if opcion == 1
+           print("Escogio sumar dos numeros")
+N1 = float(input("ingrese numero 1:."))
+N2 = float(input("ingrese numero 2:."))
+           Resultado N1+N2
+           print("El resultado de la suma es {}".format(resultado))
+           
+           
+elif opcion == 2
+           print("Escogio restar tres numeros")
+N1 = float(input("ingrese numero 1:."))
+N2 = float(input("ingrese numero 2:."))
+N3 = float(input("Ingresar numero 3:."))
+           Resultado N1-N2-N3
+           print("El resultado de la resta es {}".format(resultado))
+
+
+elif opcion == 3
+           print("Escogio multiplicar cuatro numeros")
+N1 = float(input("ingrese numero 1:."))
+N2 = float(input("ingrese numero 2:."))
+N3 = float(input("Ingresar numero 3:."))
+N4 = float(input("Ingresar numero 4:."))
+           Resultado N1*N2¨*N3*N4
+           print("El resultado de la multiplicaciones {}".format(resultado))
+
+else opcion == 4
+           print("Escogio dividir dos numeros")
+N1 = float(input("ingrese numero 1:."))
+N2 = float(input("ingrese numero 2:."))
+N3 = float(input("Ingresar numero 3:."))
+N4 = float(input("Ingresar numero 4:."))
+N5 = float(input("ingresar numero 5:."))
+           Resultado N1*N2¨*N3*N4
+           print("El resultado de la multiplicaciones {}".format(resultado))
+
+
+           
